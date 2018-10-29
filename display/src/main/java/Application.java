cef934456884ad88c22754b4e2fcee8c4e2f2823
466413559 <466413559@qq.com>
2018-10-29 09:48:53 +0800

    test html

--- display/src/main/java/Application.java
+++ display/src/main/java/Application.java
@@ -1,10 +1,16 @@
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages = "com.andidz")
 @MapperScan(basePackages = {"com.andidz.authorithy.mapper","com.andidz.bizcore.mapper"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
     public static void main(String[] args) {
         SpringApplication.run(Application.class,args);
     }
